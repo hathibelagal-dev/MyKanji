45028835dcbc5509a7632c0c441d518e10dfab3f
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-06-13 16:44:28 +0530

    Media queries

--- app/src/main/assets/kanji.js
+++ app/src/main/assets/kanji.js
@@ -1,364 +1,428 @@
 var kanji = {
-  "items": [
+  items: [
     {
-      "en": "One person, alone",
-      "jp": "一人"
+      en: "One person, alone",
+      jp: "一人",
     },
     {
-      "en": "Two people, pair",
-      "jp": "二人"
+      en: "Two people, pair",
+      jp: "二人",
     },
     {
-      "en": "Third day of month",
-      "jp": "三日"
+      en: "Third day of month",
+      jp: "三日",
     },
     {
-      "en": "Fourth day of month",
-      "jp": "四日"
+      en: "Fourth day of month",
+      jp: "四日",
     },
     {
-      "en": "Fifty five",
-      "jp": "五十五"
+      en: "Fifty five",
+      jp: "五十五",
     },
     {
-      "en": "Six times",
-      "jp": "六回"
+      en: "Six times",
+      jp: "六回",
     },
     {
-      "en": "Country",
-      "jp": "国"
+      en: "Country",
+      jp: "国",
     },
     {
-      "en": "Foreign country",
-      "jp": "外国"
+      en: "Foreign country",
+      jp: "外国",
     },
     {
-      "en": "China",
-      "jp": "中国"
+      en: "China",
+      jp: "中国",
     },
     {
-      "en": "Eight times",
-      "jp": "八回"
+      en: "Eight times",
+      jp: "八回",
     },
     {
-      "en": "Nine books",
-      "jp": "九冊の本"
+      en: "Nine books",
+      jp: "九冊の本",
     },
     {
-      "en": "Tenth day of month",
-      "jp": "十日"
+      en: "Tenth day of month",
+      jp: "十日",
     },
     {
-      "en": "One hundred men",
-      "jp": "百人の男"
+      en: "One hundred men",
+      jp: "百人の男",
     },
     {
-      "en": "Boy",
-      "jp": "男の子"
+      en: "Boy",
+      jp: "男の子",
     },
     {
-      "en": "Girl",
-      "jp": "女の子"
+      en: "Girl",
+      jp: "女の子",
     },
     {
-      "en": "Thousand",
-      "jp": "千"
+      en: "Thousand",
+      jp: "千",
     },
     {
-      "en": "Ten thousand",
-      "jp": "万"
+      en: "Ten thousand",
+      jp: "万",
     },
     {
-      "en": "Twenty",
-      "jp": "二十"
+      en: "Twenty",
+      jp: "二十",
     },
     {
-      "en": "Round, circular",
-      "jp": "円い"
+      en: "Round, circular",
+      jp: "円い",
     },
     {
-      "en": "Week",
-      "jp": "週"
+      en: "Week",
+      jp: "週",
     },
     {
-      "en": "Weekend",
-      "jp": "週末"
+      en: "Weekend",
+      jp: "週末",
     },
     {
-      "en": "Eternity; 10,000 years",
-      "jp": "万年"
+      en: "Eternity; 10,000 years",
+      jp: "万年",
     },
     {
-      "en": "All people, everyone",
-      "jp": "万人"
+      en: "All people, everyone",
+      jp: "万人",
     },
     {
-      "en": "Day",
-      "jp": "日"
+      en: "Day",
+      jp: "日",
     },
     {
-      "en": "Month",
-      "jp": "月"
+      en: "Month",
+      jp: "月",
     },
     {
-      "en": "Moonlight",
-      "jp": "月光"
+      en: "Moonlight",
+      jp: "月光",
     },
     {
-      "en": "Light",
-      "jp": "光"
+      en: "Light",
+      jp: "光",
     },
     {
-      "en": "New moon",
-      "jp": "新月"
+      en: "New moon",
+      jp: "新月",
     },
     {
-      "en": "Monday",
-      "jp": "月曜日"
+      en: "Monday",
+      jp: "月曜日",
     },
     {
-      "en": "Tuesday",
-      "jp": "火曜日"
+      en: "Tuesday",
+      jp: "火曜日",
     },
     {
-      "en": "Sunday",
-      "jp": "日曜日"
+      en: "Sunday",
+      jp: "日曜日",
     },
     {
-      "en": "Year",
-      "jp": "年"
+      en: "Year",
+      jp: "年",
     },
     {
-      "en": "Hour, time",
-      "jp": "時"
+      en: "Hour, time",
+      jp: "時",
     },
     {
-      "en": "Minute",
-      "jp": "分"
+      en: "Minute",
+      jp: "分",
     },
     {
-      "en": "Noon",
-      "jp": "午"
+      en: "Noon",
+      jp: "午",
     },
     {
-      "en": "Now",
-      "jp": "今"
+      en: "Now",
+      jp: "今",
     },
     {
-      "en": "Yesterday",
-      "jp": "昨日"
+      en: "Yesterday",
+      jp: "昨日",
     },
     {
-      "en": "Tomorrow",
-      "jp": "明日"
+      en: "Tomorrow",
+      jp: "明日",
     },
     {
-      "en": "Today",
-      "jp": "今日"
+      en: "Today",
+      jp: "今日",
     },
     {
-      "en": "Before",
-      "jp": "先"
+      en: "Before",
+      jp: "先",
     },
     {
-      "en": "Last week",
-      "jp": "先週"
+      en: "Last week",
+      jp: "先週",
     },
     {
-      "en": "Next month",
-      "jp": "来月"
+      en: "Next month",
+      jp: "来月",
     },
     {
-      "en": "Every",
-      "jp": "毎"
+      en: "Every",
+      jp: "毎",
     },
     {
-      "en": "Every day",
-      "jp": "毎日"
+      en: "Every day",
+      jp: "毎日",
     },
     {
-      "en": "Every week",
-      "jp": "毎週"
+      en: "Every week",
+      jp: "毎週",
     },
     {
-      "en": "Every time",
-      "jp": "毎回"
+      en: "Every time",
+      jp: "毎回",
     },
     {
-      "en": "What",
-      "jp": "何"
+      en: "What",
+      jp: "何",
     },
     {
-      "en": "Mother",
-      "jp": "母"
+      en: "Mother",
+      jp: "母",
     },
     {
-      "en": "Father",
-      "jp": "父"
+      en: "Father",
+      jp: "父",
     },
     {
-      "en": "Student",
-      "jp": "学生"
+      en: "Student",
+      jp: "学生",
     },
     {
-      "en": "School",
-      "jp": "学校"
+      en: "School",
+      jp: "学校",
     },
     {
-      "en": "Sea",
-      "jp": "海"
+      en: "Sea",
+      jp: "海",
     },
     {
-      "en": "Abroad",
-      "jp": "海外"
+      en: "Abroad",
+      jp: "海外",
     },
     {
-      "en": "Sunrise",
-      "jp": "日の出"
+      en: "Sunrise",
+      jp: "日の出",
     },
     {
-      "en": "Sunset",
-      "jp": "日没"
+      en: "Sunset",
+      jp: "日没",
     },
     {
-      "en": "Sunlight",
-      "jp": "日光"
+      en: "Sunlight",
+      jp: "日光",
     },
     {
-      "en": "Sunny",
-      "jp": "晴れ"
+      en: "Sunny",
+      jp: "晴れ",
     },
     {
-      "en": "Holiday, day off",
-      "jp": "休日"
+      en: "Holiday, day off",
+      jp: "休日",
     },
     {
-      "en": "Birthday",
-      "jp": "誕生日"
+      en: "Birthday",
+      jp: "誕生日",
     },
     {
-      "en": "Memory",
-      "jp": "記憶"
+      en: "Memory",
+      jp: "記憶",
     },
     {
-      "en": "To remember",
-      "jp": "思い出す"
+      en: "To remember",
+      jp: "思い出す",
     },
     {
-      "en": "Imagination",
-      "jp": "想像"
+      en: "Imagination",
+      jp: "想像",
     },
     {
-      "en": "Writer, author",
-      "jp": "作家"
+      en: "Writer, author",
+      jp: "作家",
     },
     {
-      "en": "Wind, breeze",
-      "jp": "風"
+      en: "Wind, breeze",
+      jp: "風",
     },
     {
-      "en": "Family",
-      "jp": "家族"
+      en: "Family",
+      jp: "家族",
     },
     {
-      "en": "Housewife",
-      "jp": "主婦"
+      en: "Housewife",
+      jp: "主婦",
     },
     {
-      "en": "Homeland",
-      "jp": "国家"
+      en: "Homeland",
+      jp: "国家",
     },
     {
-      "en": "Friend",
-      "jp": "友"
+      en: "Friend",
+      jp: "友",
     },
     {
-      "en": "Fire",
-      "jp": "火"
+      en: "Fire",
+      jp: "火",
     },
     {
-      "en": "Tree, wood",
-      "jp": "木"
+      en: "Tree, wood",
+      jp: "木",
     },
     {
-      "en": "Water",
-      "jp": "水"
+      en: "Water",
+      jp: "水",
     },
     {
-      "en": "Earth, ground",
-      "jp": "土"
+      en: "Earth, ground",
+      jp: "土",
     },
     {
-      "en": "Money, gold",
-      "jp": "金"
+      en: "Money, gold",
+      jp: "金",
     },
     {
-      "en": "River",
-      "jp": "川"
+      en: "River",
+      jp: "川",
     },
     {
-      "en": "Sky",
-      "jp": "空"
+      en: "Sky",
+      jp: "空",
     },
     {
-      "en": "Blue sky, clear sky",
-      "jp": "青空"
+      en: "Blue sky, clear sky",
+      jp: "青空",
     },
     {
-      "en": "Heaven",
-      "jp": "天"
+      en: "Heaven",
+      jp: "天",
     },
     {
-      "en": "Fish",
-      "jp": "魚"
+      en: "Fish",
+      jp: "魚",
     },
     {
-      "en": "Flower",
-      "jp": "花"
+      en: "Flower",
+      jp: "花",
     },
     {
-      "en": "Mountain",
-      "jp": "山"
+      en: "Mountain",
+      jp: "山",
     },
     {
-      "en": "Rain",
-      "jp": "雨"
+      en: "Rain",
+      jp: "雨",
     },
     {
-      "en": "Japanese language",
-      "jp": "日本語"
+      en: "Japanese language",
+      jp: "日本語",
     },
     {
-      "en": "Hand",
-      "jp": "手"
+      en: "Hand",
+      jp: "手",
     },
     {
-      "en": "Eye",
-      "jp": "目"
+      en: "Eye",
+      jp: "目",
     },
     {
-      "en": "Ear",
-      "jp": "耳"
+      en: "Ear",
+      jp: "耳",
     },
     {
-      "en": "Foot, feet",
-      "jp": "足"
+      en: "Foot, feet",
+      jp: "足",
     },
     {
-      "en": "Mouth",
-      "jp": "口"
+      en: "Mouth",
+      jp: "口",
     },
     {
-      "en": "House",
-      "jp": "家"
+      en: "House",
+      jp: "家",
     },
     {
-      "en": "one with bachelor's degree",
-      "jp": "学士"
+      en: "one with bachelor's degree",
+      jp: "学士",
     },
     {
-      "en": "Noon, midday",
-      "jp": "日中"
-    }
-  ]
-}
+      en: "Noon, midday",
+      jp: "日中",
+    },
+    {
+      en: "Morning",
+      jp: "朝",
+    },
+    {
+      en: "Evening",
+      jp: "夕",
+    },
+    {
+      en: "Dinner",
+      jp: "夕食",
+    },
+    {
+      en: "City",
+      jp: "市",
+    },
+    {
+      en: "Food",
+      jp: "食べ物",
+    },
+    {
+      en: "Car",
+      jp: "車",
+    },
+    {
+      en: "Book",
+      jp: "本",
+    },
+    {
+      en: "Downtown, city center",
+      jp: "市内",
+    },
+    {
+      en: "Suburbs",
+      jp: "市外",
+    },
+    {
+      en: "City resident",
+      jp: "市民",
+    },
+    {
+      en: "Breakfast",
+      jp: "朝食",
+    },
+    {
+      en: "Lunch",
+      jp: "昼食",
+    },
+    {
+      en: "Midnight",
+      jp: "深夜",
+    },
+    {
+      en: "Night sky",
+      jp: "夜空",
+    },
+    {
+      en: "Thing, item",
+      jp: "物体",
+    },
+    {
+      en: "Story, tale",
+      jp: "物語",
+    },
+  ],
+};
